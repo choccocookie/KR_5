ac99bb68b801ab7548bf21f7743627836b198458
choccocookie <Karanovvlad@gmail.com>
2024-06-29 08:47:44 +0300

    Подобран перечень компаний-работодателей, добавлен метод get_companies преобразующий указанынй перечень в список словарей companies

--- tests/HHparcer.py
+++ tests/HHparcer.py
@@ -24,7 +24,31 @@ class HH(Parser):
     def __init__(self):
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {'page': 0, 'per_page': 100}
-        self.companies_data = {}
+        self.companies_data = {
+            'Тиньков': 78638,
+            'Яндекс': 1740,
+            'Билайн': 4934,
+            'Сбербанк': 1473866,
+            'Банк ВТБ': 4181,
+            'Газпромнефть': 39305,
+            'Альфа-банк': 80,
+            'СберТех': 3529,
+            'ФинТех IQ': 5898393,
+            'Айтеко': 872178
+        }
+
+    def get_companies(self):
+        """
+            Получает имя компаний и их ID,
+            :return: список словарей с информацией о компаниях
+            """
+        companies = []
+
+        for company_name, company_id in self.companies_data.items():
+            company_url = f"https://hh.ru/employer/{company_id}"
+            company_info = {'company_id': company_id, 'company_name': company_name, 'company_url': company_url}
+            companies.append(company_info)
+        return companies
 
     def load_vacancies(self, companies):
         """Получает ID компании, по которому будет производится поиск по ссылке,
