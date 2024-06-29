f1fc0572a3a5726b26dee411759454b863d0d783
choccocookie <Karanovvlad@gmail.com>
2024-06-29 21:59:05 +0300

    Добавлены методы get_requests, get_employers, __get_vacancies_on_employer, get_all_vacancies

--- tests/HHparcer.py
+++ tests/HHparcer.py
@@ -19,11 +19,12 @@ class Parser(ABC):
 class HH(Parser):
     """
     Класс для работы с API HeadHunter
+
     """
 
     def __init__(self):
         self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'page': 0, 'per_page': 100}
+        self.params = {'page': 0, 'per_page': 10, 'sort_by': 'by_vacancies_open'}
         self.companies_data = {
             'Тиньков': 78638,
             'Яндекс': 1740,
@@ -36,6 +37,25 @@ class HH(Parser):
             'ФинТех IQ': 5898393,
             'Айтеко': 872178
         }
+
+    def __get_requests(self):
+        """
+        :return: возвращает список открытых вакансий
+        """
+        response = requests.get('https://api.hh.ru/employers', params=self.params)
+        if response.status_code == 200:
+            return response.json()['items']
+
+    def get_employers(self):
+        """
+        Проходит по списку вакансий и делает выборку id, name, url работодателей
+        :return:
+        """
+        employers = []
+        data = self.__get_requests()
+        for employer in data:
+            employers.append({"id": employer["id"], "name": employer["name"], "url": employer["url"]})
+        return employers
 
     def get_companies(self):
         """
@@ -50,6 +70,41 @@ class HH(Parser):
             companies.append(company_info)
         return companies
 
+    def __get_vacancies_on_employer(self, id):
+        """
+        :param id: id компании, параметр взят из документации к API HH
+        :return: список вакансий указанной компании
+        """
+        params = {"employer_id": id}
+        response = requests.get("https://api.hh.ru/vacancies", params=params)
+        if response.status_code == 200:
+            return response.json()["items"]
+
+    def get_all_vacancies(self):
+        """
+        через метод self.get_employers() получает список организаций, берет id организации
+        и передает в метод self.__get_vacancies_on_employer, который формирует список вакансий организации
+        в соответствии с выбранными ключами
+        """
+        employers = self.get_employers()
+        all_vacancies = []
+        for employer in employers:
+            vacancies = self.__get_vacancies_on_employer(employer["id"])
+            for vacancy in vacancies:
+                if vacancy["salary"] is None:
+                    salary_from = 0
+                    salary_to = 0
+                else:
+                    salary_from = vacancy["salary"]["from"] if vacancy["salary"]["from"] else 0
+                    salary_to = vacancy["salary"]["to"] if vacancy["salary"]["to"] else 0
+                all_vacancies.append({"id": vacancy["id"], "name": vacancy["name"],
+                                      "url": vacancy["alternate_url"],
+                                      "salary_from": salary_from,
+                                      "salary_to": salary_to,
+                                      "employer": employer["id"],
+                                      "area":vacancy["area"]["name"]})
+        return all_vacancies
+
     def load_vacancies(self, companies):
         """Получает ID компании, по которому будет производится поиск по ссылке,
         циклично забирает данные по ссылке с переменной company_id и добвляет их в словарь self.vacancies"""
@@ -59,5 +114,8 @@ class HH(Parser):
             response = requests.get(f"https://api.hh.ru/vacancies?employer_id={company_id}", headers=self.headers,
                                     params=self.params)
             self.vacancies = response.json()['items']
-            #print(self.vacancies)
         return self.vacancies
+
+
+hh = HH()
+print(hh.get_employers())
