5f98d40a2e367b1a5b5566db752e0b1c8c2e6b24
choccocookie <Karanovvlad@gmail.com>
2024-06-29 08:33:21 +0300

    Добавлен метод класса load_vacancies для получения вакансий

--- tests/HHparcer.py
+++ tests/HHparcer.py
@@ -25,3 +25,15 @@ class HH(Parser):
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {'page': 0, 'per_page': 100}
         self.companies_data = {}
+
+    def load_vacancies(self, companies):
+        """Получает ID компании, по которому будет производится поиск по ссылке,
+        циклично забирает данные по ссылке с переменной company_id и добвляет их в словарь self.vacancies"""
+
+        for co in companies:
+            company_id = co['company_id']
+            response = requests.get(f"https://api.hh.ru/vacancies?employer_id={company_id}", headers=self.headers,
+                                    params=self.params)
+            self.vacancies = response.json()['items']
+            #print(self.vacancies)
+        return self.vacancies
