02771d8f91049fc711245f359a4445119f020342
choccocookie <Karanovvlad@gmail.com>
2024-06-30 18:14:34 +0300

    Исправлена функция create database Добавлена функция создания таблиц Добавлена функция заполнения таблиц Заполнен config для подключения к БД

--- src/config.py
+++ src/config.py
@@ -10,7 +10,8 @@ def config(filename='database.ini', section='postgresql'):
         for param in params:
             db[param[0]] = param[1]
     else:
-        raise Exception()
+        raise Exception("Section {0} not found in the {1} file".format(section, filename))
+
 
     return db
 
