c141a5a56ebe34f3fa34f0487ebf0d0a2b948073
choccocookie <Karanovvlad@gmail.com>
2024-06-30 12:44:08 +0300

    добавлена функция заполнения таблиц с организациями и вакансиями

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import psycopg2
+from HHparcer import HH
 
 
 def create_database(name, params):
@@ -13,22 +14,22 @@ def create_database(name, params):
 
         conn = psycopg2.connect(dbname=name, **params)
         with conn.cursor() as cur:
-            cur.execute(f'CREATE TABLE IF NOT EXISTS employers '
-                        f'(employer_id int PRIMARY KEY, '
-                        f'employer_name varchar(100) UNIQUE NOT NULL, '
-                        f'employer_url varchar (100))')
+            cur.execute("""CREATE TABLE IF NOT EXISTS employers
+                        (employer_id int PRIMARY KEY,
+                        employer_name varchar(100) UNIQUE NOT NULL,
+                        employer_url varchar (100))
+                        """)
         with conn.cursor() as cur:
-            cur.execute(f'CREATE TABLE IF NOT EXISTS vacancies '
-                        f'(vacancy_id int PRIMARY KEY, '
-                        f'vacancy_name varchar (100) NOT NULL,'
-                        f'vacancy_url varchar (100) NOT NULL'
-                        f'salary_from int,'
-                        f'salary_to int,'
-                        f'employer int REFERENCES employers (varchar(100),'
-                        f'salary_from int, '
-                        f'salary_to int,'
-                        f'description text,'
-                        f'requirement text)')
+            cur.execute("""CREATE TABLE IF NOT EXISTS vacancies
+                        (vacancy_id int PRIMARY KEY,
+                        vacancy_name varchar (100) NOT NULL,
+                        vacancy_url varchar (100) NOT NULL,
+                        salary_from int,
+                        salary_to int,
+                        employer int REFERENCES employers (varchar(100),
+                        description text,
+                        requirement text)
+                        """)
         conn.commit()
         conn.close()
 
@@ -36,3 +37,25 @@ def create_database(name, params):
 
     except Exception as e:
         return f"Произошла ошибка: {e}"
+
+def insert_data(name, params):
+    """Сохранение данных о компаниях и вакансиях в базу данных"""
+    hh = HH()
+    employers = hh.get_employers()
+    vacancies = hh.get_all_vacancies()
+    conn = psycopg2.connect(dbname='postgres', **params)
+    with conn.cursor() as cur:
+        for employer in employers:
+            cur.execute(
+                """
+                INSERT INTO employers VALUES (%s, %s, %s)
+                """, (employer["id"], employer["name"], employer["url"]))
+        for vacancy in vacancies:
+            cur.execute(
+                """
+                INSERT INTO vacancies (vacancy_id, vacancy_name, vacancy_url, salary_from, salary_to, employer, description, requirement)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s),
+                """, (vacancy["id"], vacancy["name"], vacancy["url"], vacancy["salary_from"], vacancy["salary_to"],
+                      vacancy["employer"], vacancy["area"], vacancy["description"], vacancy["requirement"]))
+        conn.commit()
+        conn.close()
